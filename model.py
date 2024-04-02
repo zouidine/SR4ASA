3595f29e9e731743c16b52a7b40a88b07af78369
zouidine <48861700+zouidine@users.noreply.github.com>
2024-04-02 11:48:24 +0000

    Update model.py

--- model.py
+++ model.py
@@ -4,13 +4,14 @@ import torch.nn.functional as F
 import torch.nn as nn
 import torch
 
-
 class CNN_RNN(nn.Module):
     def __init__(self, bert, hid_dim, n_filters, filter_sizes, dropout, device):
         super().__init__()
 
         self.hid_dim = hid_dim
         self.device = device
+        self.saved_log_probs = []
+        self.selected_tokens = []
         self.nbr_tokens = 0
 
         self.embedding = bert
@@ -36,45 +37,46 @@ class CNN_RNN(nn.Module):
 
     def select_policy(self, embedded, mask, training):
         #embedded = [batch size, sent len, emb dim]
-        self.saved_log_probs = torch.zeros(mask.shape, dtype=torch.float)
-        self.selected_tokens = torch.zeros(mask.shape)
         lens = torch.sum(mask, dim=1).tolist()
-        self.batch_nbr_tokens = 0
-
-        hid = torch.zeros(mask.shape[0], self.hid_dim, requires_grad=True)
-        hid = self.gru_cell(embedded[:,0,:].detach(), hid.to(self.device))
-        self.selected_tokens[:,0] = torch.ones(mask.shape[0])
-        for i in range(1,max(lens)-1):
-            hid = self.gru_cell(embedded[:,i,:].detach(), hid)
-            probs = self.select_linear(hid)
-            if (training):
-                probs = F.softmax(probs, dim=1)
-                categ = Categorical(probs)
-                k = categ.sample()
-                self.saved_log_probs[:,i]  = categ.log_prob(k)
-            else: k = probs.argmax(dim=1)
-            self.selected_tokens[:,i] = k
+        for eps in range(embedded.shape[0]):
+            if (training): self.saved_log_probs.append([])
+            self.selected_tokens.append([])
+            hid = torch.zeros(self.hid_dim, requires_grad=True,
+                              device=self.device)
+            hid = self.gru_cell(embedded[eps,0,:].detach(), hid)
+            self.selected_tokens[-1].append(1)
+            for i in range(1,lens[eps]-1):
+                hid = self.gru_cell(embedded[eps,i,:].detach(), hid)
+                probs = self.select_linear(hid)
+                if (training):
+                    probs = F.softmax(probs, dim=0)
+                    categ = Categorical(probs)
+                    k = categ.sample()
+                    self.saved_log_probs[-1].append(categ.log_prob(k))
+                else: k = probs.argmax()
+                self.selected_tokens[-1].append(k.item())
+            self.selected_tokens[-1].append(1)
 
         new_embedded = torch.zeros(embedded.shape, dtype=embedded.dtype,
                                    device=self.device)
-        new_mask = torch.zeros(mask.shape, dtype=mask.dtype)
+        new_mask = torch.zeros(mask.shape, dtype=mask.dtype, device=self.device)
         for i in range(new_embedded.shape[0]):
             k = 0
-            new_mask[i,:lens[i]-1] = self.selected_tokens[i,:lens[i]-1]
-            new_mask[i,lens[i]-1] = 1
+            new_mask[i,0:lens[i]] = torch.tensor(self.selected_tokens[i])
             for j in range(lens[i]):
-                if new_mask[i][j]==1:
+                if self.selected_tokens[i][j]==1:
                     new_embedded[i,k,:] = embedded[i,j,:].detach()
                     k += 1
-        self.batch_nbr_tokens = torch.sum(new_mask, dim=1).to('cpu')
         return new_embedded, new_mask
 
     def forward(self, text, mask, training=True):
+        del self.saved_log_probs[:]
+        del self.selected_tokens[:]
         #text = [batch size, sent len]
         with torch.no_grad():
             embedded = self.embedding(text, attention_mask=mask)[0]
         embedded, mask = self.select_policy(embedded, mask, training)
-        embedded = pack_padded_sequence(embedded, torch.sum(mask, 1),
+        embedded = pack_padded_sequence(embedded, torch.sum(mask, 1).to('cpu'),
                                         batch_first=True, enforce_sorted=False)
         #embedded = [batch size, sent len, emb dim]
         output, (_, _) = self.rnn(embedded)
