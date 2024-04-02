10ce1ec524c10a7351a6923508fac760473e5bdd
zouidine <48861700+zouidine@users.noreply.github.com>
2024-04-02 12:08:33 +0000

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -9,7 +9,7 @@ def get_scores(preds, y):
     f1s = f1_score(y, preds)
     return acc, pre, rec, f1s
 
-def train(model, iterator, optimizer, criterion, epoch_no):
+def train(model, iterator, optimizer, criterion, epoch_no, device, zeta=0.25):
     epoch_loss = 0
     epoch_acc = 0
     model.train()
@@ -17,7 +17,23 @@ def train(model, iterator, optimizer, criterion, epoch_no):
         for batch_no, batch in enumerate(it, start=1):
             optimizer.zero_grad()
             predictions = model(batch[0].to(device), batch[1].to(device))
-            loss = criterion(predictions, batch[2].to(device))
+            bce_loss = criterion(predictions, batch[2].to(device))
+            pg_loss = torch.tensor(0.0, device=device, requires_grad=True)
+            for i in range(predictions.shape[0]):
+                x1 = sum(model.selected_tokens[i])
+                model.nbr_tokens += x1
+                x2 = torch.sum(batch[1][i]).item()
+                nst = x1/x2
+                if (torch.round(torch.sigmoid(predictions[i]))==batch[2][i]):
+                    if nst<zeta:
+                        R = 1
+                    else:
+                        R = 1 + (1-nst)
+                else: R = -1
+                log_probs = model.saved_log_probs[i]
+                for log_prob in log_probs:
+                    pg_loss = pg_loss + log_prob*R
+            loss = bce_loss - pg_loss/predictions.shape[0]
             loss.backward(retain_graph=True)
             optimizer.step()
             acc, _, _, _ = get_scores(torch.round(torch.sigmoid(predictions)).tolist(),
@@ -33,7 +49,7 @@ def train(model, iterator, optimizer, criterion, epoch_no):
                         refresh=True,
                     )
 
-def evaluate(model, iterator):
+def evaluate(model, iterator, device):
     model.eval()
     pred = []
     targ = []
